4ecd0f1c4c1d641d8639c93441a05eac78b80ac6
Muqsith <muqsithali8@gmail.com>
2025-03-01 00:53:12 +0530

    suppress warnings

--- src/main/java/com/example/springAI/controller/ChatController.java
+++ src/main/java/com/example/springAI/controller/ChatController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ChatController {
 
+    @SuppressWarnings("unused")
     private final EmailReaderService emailReaderService;
     private final EmailService emailService;
     private final ChatClient chatClient;
